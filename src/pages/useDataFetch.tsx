7f4f394d2686346e0772bedb323a59ab7e09a37b
kader009 <kadermollarasel000@gmail.com>
2024-12-07 16:45:40 +0600

    use types for better safety

--- src/pages/useDataFetch.tsx
+++ src/pages/useDataFetch.tsx
@@ -9,11 +9,41 @@ async function fetchData() {
 
 const data = fetchData();
 
+export interface Root {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
 const UseDataFetch = () => {
   const users = use(data);
   return (
     <div className="flex justify-center items-center">
-      {users.map((user) => (
+      {users.map((user: Root) => (
         <div key={user.id}>
           <h1 className="bg-black text-white m-2 p-1 rounded">{user.name}</h1>
         </div>
