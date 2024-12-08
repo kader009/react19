72dc1444e81198a4cb7c5bb2981b0af0ccc82c74
kader009 <kadermollarasel000@gmail.com>
2024-12-08 22:51:19 +0600

    import usetransition hook

--- src/pages/UseTransition.tsx
+++ src/pages/UseTransition.tsx
@@ -1,11 +1,15 @@
+import { useState, useTransition } from 'react';
 
 const UseTransition = () => {
+  const [name, SetName] = useState('');
+  const [error, SetError] = useState(null);
+  const [isPending, startTransition] = useTransition()
   return (
     <div>
-      <input type="text" className="bg-green-500 rounded outline-none p-2"/>
+      <input type="text" className="bg-green-500 rounded outline-none p-2" />
       <button className="bg-black text-white rounded p-2">Submit</button>
     </div>
-  )
-}
+  );
+};
 
-export default UseTransition
+export default UseTransition;
