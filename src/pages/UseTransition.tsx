549b00ba19e2b566dd7243b60ca2fe5c107d1833
kader009 <kadermollarasel000@gmail.com>
2024-12-08 22:58:09 +0600

    create update function

--- src/pages/UseTransition.tsx
+++ src/pages/UseTransition.tsx
@@ -1,13 +1,29 @@
 import { useState, useTransition } from 'react';
 
 const UseTransition = () => {
-  const [name, SetName] = useState('');
-  const [error, SetError] = useState(null);
-  const [isPending, startTransition] = useTransition()
+  function UpdateName({}){
+
+    const [name, SetName] = useState('');
+    const [error, SetError] = useState(null);
+    const [isPending, startTransition] = useTransition();
+    console.log(name);
+  
+    const handleUpdate =() =>{
+      startTransition(async () =>{
+        const error = await
+      })
+    }
+  }
   return (
     <div>
-      <input type="text" className="bg-green-500 rounded outline-none p-2" />
-      <button className="bg-black text-white rounded p-2">Submit</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => SetName(e.target.value)}
+        className="bg-green-500 rounded outline-none p-2"
+      />
+      <button type='submit' className="bg-black text-white rounded p-2">Update</button>
+      {error && <p>{error}</p>}
     </div>
   );
 };
