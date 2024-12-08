2fb4a0783fd442b0b3f6b5efe57b548cd3c85cf4
kader009 <kadermollarasel000@gmail.com>
2024-12-08 22:49:22 +0600

    take input and button

--- src/pages/UseTransition.tsx
+++ src/pages/UseTransition.tsx
@@ -1,7 +1,10 @@
 
 const UseTransition = () => {
   return (
-    <div>UseTransition</div>
+    <div>
+      <input type="text" className="bg-green-500 rounded outline-none p-2"/>
+      <button className="bg-black text-white rounded p-2">Submit</button>
+    </div>
   )
 }
 
