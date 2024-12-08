f5eb5ca0809373d13349cb8313f5775e5dff5613
kader009 <kadermollarasel000@gmail.com>
2024-12-08 23:57:44 +0600

    correct the button problem

--- src/pages/UseTransition.tsx
+++ src/pages/UseTransition.tsx
@@ -8,7 +8,7 @@ const UseTransition = () => {
   const UpdateName = async (name: string): Promise<string | null> => {
     return name.length < 3 ? 'name must a 3 character long' : null;
   };
-  
+
   const handleUpdate = () => {
     startTransition(async () => {
       const errors = await UpdateName(name);
@@ -16,8 +16,11 @@ const UseTransition = () => {
         SetError(error);
         return;
       }
+      SetError(null);
+      console.log(`name updated:`, name);
     });
   };
+
   return (
     <div>
       <input
@@ -27,14 +30,14 @@ const UseTransition = () => {
         className="bg-green-500 rounded outline-none p-2"
       />
       <button
-        onSubmit={handleUpdate}
+        onClick={handleUpdate}
         disabled={isPending}
         type="submit"
         className="bg-black text-white rounded p-2"
       >
-        Update
+        {isPending ? 'Updating..' : 'Update'}
       </button>
-      {error && <p>{error}</p>}
+      {error && <p className='text-red-600'>{error}</p>}
     </div>
   );
 };
