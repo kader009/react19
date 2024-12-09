17397e7ef4bb206c5cfb64324ac94b1413a5f5a0
kader009 <kadermollarasel000@gmail.com>
2024-12-09 19:02:25 +0600

    back to previous mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ReactCompiler from 'babel-plugin-react-compiler';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel:{
-      plugins:[ReactCompiler]
-    }
-  })],
+  plugins: [react()],
 })
