e650eadda51098c2a1d0f04a071928469cddb0cc
kader009 <kadermollarasel000@gmail.com>
2024-12-09 20:27:54 +0600

    create custom file

---
+++ src/pages/CustomElement.tsx
@@ -0,0 +1,9 @@
+import { useEffect, useRef } from "react";
+
+const CustomElement = () => {
+  const custom = useRef('')
+  useEffect(() =>{},[])
+  return <div>CustomElement</div>;
+};
+
+export default CustomElement;
