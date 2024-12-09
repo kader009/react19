cbe2063ded81a94c78cea833efbf6a17313ce796
kader009 <kadermollarasel000@gmail.com>
2024-12-09 19:33:40 +0600

    title change with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useEffect } from 'react';
 import UseDataFetch from './pages/useDataFetch';
 import UseTransition from './pages/UseTransition';
-
 const App = () => {
+  useEffect(() => {
+    document.title = 'home page';
+    return () => {
+      document.title = 'product';
+    };
+  }, []);
   return (
     <div>
       <UseDataFetch />
       <br />
       <br />
-      <UseTransition/>
+      <UseTransition />
     </div>
   );
 };
