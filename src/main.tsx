0a6e7259657a7de7988682bd754c31f33b514758
kader009 <kadermollarasel000@gmail.com>
2024-12-13 19:35:33 +0600

    route setup for dashbaord

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import About from './pages/About.tsx';
+import Home from './pages/home/Home.tsx';
+import Setting from './pages/home/Setting.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +13,10 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
+        <Route path="dashboard">
+          <Route index element={<Home />} />
+          <Route path="setting" element={<Setting />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
