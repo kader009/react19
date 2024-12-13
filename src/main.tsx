a01c2423a9de71fde20cde74dfbf6b6f7d8ffb6e
kader009 <kadermollarasel000@gmail.com>
2024-12-13 19:02:15 +0600

    create vercel file

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
+import About from './pages/About.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes />
-      <Route path="/" element={<App />} />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
