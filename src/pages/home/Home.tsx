117a41ad0c41fc34d453d7cc9b781515232439b3
kader009 <kadermollarasel000@gmail.com>
2024-12-13 19:32:27 +0600

    create setting page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div>Home Page</div>;
+  return <div>Home Page for dashboard</div>;
 };
 
 export default Home;
---
+++ src/pages/home/Setting.tsx
@@ -0,0 +1,5 @@
+const Setting = () => {
+  return <div>this is Setting page dashbaord</div>;
+};
+
+export default Setting;
