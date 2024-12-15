a15a5b98285df71cef27cc27c3707549c90d7f33
kader009 <kadermollarasel000@gmail.com>
2024-12-15 17:56:51 +0600

    add new p tag inside div

--- src/pages/home/Setting.tsx
+++ src/pages/home/Setting.tsx
@@ -1,5 +1,5 @@
 const Setting = () => {
-  return <div>this is Setting page for dashbaord only</div>;
+  return <div>this is Setting page for dashbaord only <p>this is awesome</p></div>;
 };
 
 export default Setting;
